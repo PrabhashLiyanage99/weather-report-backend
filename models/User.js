0f99acf3907e6d96a1dd8ca9d6d012c0d374b9d1
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2025-03-15 17:26:06 +0530

    impliment user features

--- models/User.js
+++ models/User.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
-const UserScheme = new mongoose.Schema({
-    name:{type: String, required: true},
-    email:{type: String, required: true, unique: true},
-    password:{ type: String, required: true},
+const UserSchema = new mongoose.Schema({
+    name:{
+        type: String, 
+        required: true
+    },
+    email:{
+        type: String, 
+        required: true,
+         unique: true
+    },
+    password:{
+        type: String,
+         required: true
+    },
 });
 
-module.exports = mongoose.model('User', UserScheme);
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/dbConfig');
+const userRoutes = require('./routes/userRoutes');
 const app = express();
 connectDB();
 require("dotenv").config();
 
 app.use(cors());
 app.use(bodyParser.json());
-;
 
-const PORT = process.env.PORT || 5001;
+app.use("/api/users",userRoutes)
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
