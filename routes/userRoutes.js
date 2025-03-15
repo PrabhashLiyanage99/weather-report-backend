906af8d9f0e500af99f2cb2041f19eb51604f575
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2025-03-15 22:41:06 +0530

    impliment wether routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { registerUser, getUsers } = require('../controllers/userController');
-
 const router = express.Router();
 
 router.post('/register', registerUser);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/dbConfig');
 const userRoutes = require('./routes/userRoutes');
+const weatherRoutes = require('./routes/weatherRoutes');
 const app = express();
 connectDB();
 require("dotenv").config();
@@ -10,7 +11,8 @@ require("dotenv").config();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api/users",userRoutes)
+app.use("/api/users",userRoutes);
+app.use('/api/weather',weatherRoutes);
 
 const PORT = process.env.PORT || 5000;
 
