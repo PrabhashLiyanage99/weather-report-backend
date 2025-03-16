2480e274afb013bab29abf350cff654f06491958
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2025-03-16 11:21:53 +0530

    impliment get wether details with date and time

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,16 +18,7 @@ const registerUser = async(req, res) => {
     }
 };
 
-const getUsers = async(req, res) => {
-    try{
-        const users = await User.find();
-        res.json(users);
-    }catch(error){
-        res.status(500).json({ error: error.message});
-    }
-};
 
 module.exports ={
     registerUser,
-    getUsers
 }
